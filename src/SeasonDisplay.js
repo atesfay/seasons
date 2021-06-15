bea3a9ce16efde39951ebd059701adf113a0e027
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2021-06-15 16:33:02 -0700

    Add CSS element to Season Display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
@@ -27,11 +28,13 @@ const SeasonDisplay =(props) => {
 
     const {text, iconName} = seasonConfig[season] // {text, iconName}
 
-    return <div>
-               < i className={`${iconName} icon`} />
+    return (
+        <div className={`season-display ${season}`}>
+               < i className={`icon-left massive ${iconName} icon`} />
                <h1>{text}</h1> 
-               < i className={`${iconName} icon`} />
-           </div>
+               < i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
