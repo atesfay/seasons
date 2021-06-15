ecf6dcd4d92cbb706c4c7607735155acfea85b0c
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2021-06-15 15:33:38 -0700

    Fix conflict error

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+    Summer: {
+        text: "It's Summer, Let's hit the beach!",
+        iconName: 'sun'
+    },
+    Winter:{
+        text: "It's Winter, it's soo cold!",
+        iconName: 'snowflake'
+    }
+} ;
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9 ){
         return lat > 0 ? 'Summer' : 'Winter';
@@ -13,13 +24,13 @@ const SeasonDisplay =(props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     console.log(props.lat);
     console.log(season);
-    const text = season === 'Winter' ? 'Burr, it is chilly' : 'lets hit the beach';
-    const icon = season === 'Winter' ? 'Snowflake' : 'Sun';
+
+    const {text, iconName} = seasonConfig[season] // {text, iconName}
 
     return <div>
-               < i className={`${icon} icon`} />
-               <h1>It's {season}, {text}</h1> 
-               < i className={`${icon} icon`} />
+               < i className={`${iconName} icon`} />
+               <h1>{text}</h1> 
+               < i className={`${iconName} icon`} />
            </div>
 };
 
